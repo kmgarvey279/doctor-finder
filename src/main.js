676bb2881f8ea244e1bc06755531f3eb6eb45ec5
Kevin Garvey <kmgarvey279@gmail.com>
2019-06-07 16:04:32 -0700

    Add ability to return additional doctor info

--- src/main.js
+++ src/main.js
@@ -17,7 +17,23 @@ $(document).ready(function() {
       for(let i = 0; i < body.data.length; i++) {
         let firstName = `${body.data[i].profile.first_name}`;
         let lastName = `${body.data[i].profile.last_name}`;
-        $('#results').append(lastName + "," + firstName + "<br>");
+        let addressStreet = `${body.data[i].practices[0].visit_address.street}`;
+        let addressCity = `${body.data[i].practices[0].visit_address.city}`;
+        let addressState = `${body.data[i].practices[0].visit_address.state}`;
+        let phone = `${body.data[i].practices[0].phones[0].number}`;
+        let website = `${body.data[i].practices[0].website}`;
+        if(website == "undefined") {
+          website = "n/a";
+        }
+        let newPatientsBool = `${body.data[i].practices[0].accepts_new_patients}`;
+        console.log(newPatientsBool);
+        let newPatientsStr = "";
+        if(newPatientsBool === "true") {
+          newPatientsStr = "This doctor is currently accepting new patients."
+        } else {
+          newPatientsStr = "This doctor is not currently accepting new patients."
+        }
+        $('#results').append("Name: " + lastName + ", " + firstName + "<br>" + "Address: " + addressStreet + "<br>" + addressCity + "," + addressState + "<br>" + "Phone Number: " + phone + "<br>" + "Website: " + website + "<br>" + newPatientsStr + "<br><br>");
       }
     }, function(error) {
       $('showErrors').text(`There was an error processing your request: ${error.message}`);
