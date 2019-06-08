32128b8b9e267c68c80bd54ed980c06afd26b50b
Kevin Garvey <kmgarvey279@gmail.com>
2019-06-08 14:50:57 -0700

    add additional lookup options

--- src/doctor-finder.js
+++ src/doctor-finder.js
@@ -1,8 +1,32 @@
 export class DoctorFinder {
+  constructor() {
+    this.location = "";
+  }
+
+  setLocation(newLocation) {
+    this.location = newLocation;
+  }
+
+  getDoctorBySpecialty(specialty) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=${specialty}&location=${this.location}&skip=0&limit=20&user_key=${process.env.DOC_KEY}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+
   getDoctorByCondition(condition) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=wa-seattle&skip=0&limit=20&user_key=${process.env.DOC_KEY}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=${this.location}&skip=0&limit=20&user_key=${process.env.DOC_KEY}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -17,7 +41,7 @@ export class DoctorFinder {
   getDoctorByName(firstName, lastName) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&location=wa-seattle&skip=0&limit=20&user_key=${process.env.DOC_KEY}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&location=${this.location}&skip=0&limit=20&user_key=${process.env.DOC_KEY}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -7,18 +7,21 @@ import './css/styles.css';
 function showResults(body) {
   $('#results').empty();
   if(body.data.length == 0) {
-    $('#results').append("No doctors matched your search. Please update your criteria and try again.");
+    $('#results').append("No doctors were found. Please update your criteria and try again.");
   }
   for(let i = 0; i < body.data.length; i++) {
     let firstName = `${body.data[i].profile.first_name}`;
     let lastName = `${body.data[i].profile.last_name}`;
-    let addressStreet = `${body.data[i].practices[0].visit_address.street}`;
-    let addressCity = `${body.data[i].practices[0].visit_address.city}`;
-    let addressState = `${body.data[i].practices[0].visit_address.state}`;
-    let phone = `${body.data[i].practices[0].phones[0].number}`;
-    let website = `${body.data[i].practices[0].website}`;
-    if(website == "undefined") {
-      website = "n/a";
+    for(let j = 0; j < body.data[i].practices.length; j++){
+      let practiceName = `${body.data[i].practices[j].name}`;
+      let addressStreet = `${body.data[i].practices[j].visit_address.street}`;
+      let addressCity = `${body.data[i].practices[j].visit_address.city}`;
+      let addressState = `${body.data[i].practices[j].visit_address.state}`;
+      let phone = `${body.data[i].practices[j].phones[j].number}`;
+      let website = `${body.data[i].practices[j].website}`;
+      if(website == "undefined") {
+        website = "n/a";
+      }
     }
     let newPatientsBool = `${body.data[i].practices[0].accepts_new_patients}`;
     console.log(newPatientsBool);
@@ -28,17 +31,63 @@ function showResults(body) {
     } else {
       newPatientsStr = "This doctor is not currently accepting new patients."
     }
-    $('#results').append("Name: " + lastName + ", " + firstName + "<br>" + "Address: " + addressStreet + "<br>" + addressCity + "," + addressState + "<br>" + "Phone Number: " + phone + "<br>" + "Website: " + website + "<br>" + newPatientsStr + "<br><br>");
+    $('#results').append("Name: " + lastName + ", " + firstName + "<br>" + "Practice: " + practiceName + "<br>" + "Address: " + addressStreet + "<br>" + addressCity + "," + addressState + "<br>" + "Phone Number: " + phone + "<br>" + "Website: " + website + "<br>" + newPatientsStr + "<br><br>");
   }
 }
+
+function populateSpecialties() {
+  let promise = new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+    let url = `https://api.betterdoctor.com/2016-03-01/specialties?user_key=${process.env.DOC_KEY}`;
+    request.onload = function() {
+      if (this.status === 200) {
+        resolve(request.response);
+      } else {
+        reject(Error(request.statusText));
+      }
+    }
+    request.open("GET", url, true);
+    request.send();
+  });
+  promise.then(function(response) {
+    let body = JSON.parse(response);
+    for(let i = 0; i < body.data.length; i++){
+      $("#dropdown-specialties").append('<option value="' + body.data[i].uid + '">' + body.data[i].name + '</option>');
+    }
+  }, function(error) {
+    $('showErrors').text(`There was an error processing your request: ${error.message}`);
+  });
+}
+
+let doctorFinder = new DoctorFinder();
+
 $(document).ready(function() {
+  $("#user-location").click(function() {
+    let state = $('#location-state').val();
+    let city $('#location-city').val().toLowerCase();
+    let location = state + "-" + city
+    $('#location-state').val("");
+    $('#location-city').val("");
+    doctorFinder.setLocation(location);
+    populateSpecialties();
+  });
+
+  $("#doctorSpecialty").click(function() {
+    let specialty = $('#specialty').val();
+    $('#specialty').val("");
+    let promise = doctorFinder.getDoctorBySpecialty(specialty);
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      showResults(body);
+    }, function(error) {
+      $('showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
+
   $("#doctorCondition").click(function() {
     let condition = $('#condition').val();
     $('#condition').val("");
-
-    let doctorFinder = new DoctorFinder();
     let promise = doctorFinder.getDoctorByCondition(condition);
-
     promise.then(function(response) {
       let body = JSON.parse(response);
       showResults(body);
@@ -52,10 +101,7 @@ $(document).ready(function() {
     let lastName = $('#last-name').val();
     $('#first-name').val("");
     $('#last-name').val("");
-
-    let doctorFinder = new DoctorFinder();
     let promise = doctorFinder.getDoctorByName(firstName, lastName);
-
     promise.then(function(response) {
       let body = JSON.parse(response);
       showResults(body);
